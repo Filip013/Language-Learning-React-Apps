e6edafd5813195445c3f17c1776eb8cab854bea1
filip013 <filip013@gmail.com>
2026-07-31 21:12:33 +0200

    Fix deep link multi-instance bug using tauri-plugin-single-instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,15 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
+  let mut builder = tauri::Builder::default();
+
+  #[cfg(desktop)]
+  {
+      builder = builder.plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
+          println!("New instance opened with args: {argv:?}");
+      }));
+  }
+
+  builder
     .plugin(tauri_plugin_deep_link::init())
     .plugin(tauri_plugin_opener::init())
     .setup(|app| {
