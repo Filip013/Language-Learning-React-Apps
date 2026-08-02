248ca1e27600eb1baedaa1a349a832552c096d43
Filip013 <filip013@gmail.com>
2026-08-02 08:41:57 +0200

    Remove flutter_app from main branch (preserved on flutter-port branch)

--- flutter_app/lib/services/web_audio_player.dart
+++
@@ -1,3 +0,0 @@
-// lib/services/web_audio_player.dart
-export 'web_audio_player_stub.dart'
-    if (dart.library.js_interop) 'web_audio_player_web.dart';
--- flutter_app/lib/services/web_audio_player_stub.dart
+++
@@ -1,8 +0,0 @@
-// lib/services/web_audio_player_stub.dart
-import 'dart:typed_data';
-
-class WebAudioChunkPlayer {
-  void init() {}
-  void playChunk(Uint8List pcmBytes) {}
-  void stop() {}
-}
